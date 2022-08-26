135418e9196b6199b41850d627508d07f41c1f99
山室友樹 <yuki.sumaho@gmail.com>
2022-08-26 17:30:53 +0900

    chore(contract): set hardhat network

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,6 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
   solidity: "0.8.9",
 };
 
